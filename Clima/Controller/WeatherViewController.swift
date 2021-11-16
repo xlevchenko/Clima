e339f00a905739a9921c1771eca39e2fcdd76af8
Olexsii <realcarter50@gmail.com>
2021-11-16 21:27:25 +0200

    update file

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -2,9 +2,6 @@
 //  ViewController.swift
 //  Clima
 //
-//  Created by Angela Yu on 01/09/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
-//
 
 import UIKit
 import CoreLocation
--- Clima/SceneDelegate.swift
+++ Clima/SceneDelegate.swift
@@ -1,10 +1,7 @@
 //
 //  SceneDelegate.swift
 //  Clima
-//
-//  Created by Angela Yu on 01/09/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
-//
+
 
 import UIKit
 
